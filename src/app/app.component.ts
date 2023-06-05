6fa12afe31fb0f7cc2fafb5e8f0ef2ab42eb6b6e
Alifoxy <nightfoxy666@gmail.com>
2023-06-05 11:27:47 +0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  num = 150
+  flag = false
+  inc() {
+    this.num += 100
+  }
+}
