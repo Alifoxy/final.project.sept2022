1cdb53f0a325b22c89d9b8f175c48f4c30ca226a
Alifoxy <nightfoxy666@gmail.com>
2023-06-08 01:24:39 +0300

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '<router-outlet></router-outlet>',
 })
 export class AppComponent {
-  num = 150
-  flag = false
-  inc() {
-    this.num += 100
-  }
+  title = 'MainPrj2023';
 }
