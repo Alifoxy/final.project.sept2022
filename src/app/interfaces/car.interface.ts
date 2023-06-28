e672c4468fa40c4f54047a0bd50e40c20fece501
Alifoxy <nightfoxy666@gmail.com>
2023-06-28 15:56:00 +0300

    fixed errors

--- src/app/interfaces/car.interface.ts
+++ src/app/interfaces/car.interface.ts
@@ -1,6 +1,19 @@
 export interface ICar {
   id: number;
-  brand: string;
-  price: number;
-  year: number;
+  name: string;
+  surname: string;
+  email: string;
+  phone: number;
+  age: number;
+  course: string;
+  course_format: string;
+  course_type: string;
+  status: string;
+  sum: number;
+  already_paid: number;
+  group: string;
+  created_at: string;
+  manager: string;
+
+
 }
